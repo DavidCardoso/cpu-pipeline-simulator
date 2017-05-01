9c4117264e8047bc3ba8fac9ed1d789714907224
davidcardoso-ti <davidcardoso.ti@gmail.com>
2017-05-01 00:32:20 -0300

    - validando parâmetros passados via linha de comando - validando arquivo de entrada (tipo e tamanho) - lendo arquivo de entrada - validando instruções permitidas - preenchendo lista e dicionário de instruções - preenchendo dicionário de labels

--- simulador_pipeline/pipeline.py
+++ simulador_pipeline/pipeline.py
@@ -9,15 +9,16 @@
 #	@date		03/05/2017
 #	@author		David Cardoso
 #	@copyright	2017 - All rights reserveds
-#	@sa 		http://projetos.imd.ufrn.br/davidcardoso-ti/imd0703/blob/master/hmac/guard.py
+#	@sa 		http://projetos.imd.ufrn.br/davidcardoso-ti/imd0041/blob/master/simulador_pipeline/pipeline.py
 
 import os, sys			# os, sys 		- recursos de sistema
 import shutil 			# shutil 		- operações avançadas de sistema
 import mmap 			# mmap 			- suporte à arquivo em memória
+import collections		# collections 	- estrutura de dados
+import string 			# string 		- string
 
-from collections import OrderedDict		# OrderedDict 	- dicionário ordenado
 from datetime import datetime			# datetime 		- data e tempo
-from string import split 				# split 		- separar strings de acordo com separador
+from time import sleep 					# sleep 		- pausa a execução da thread atual
 
 """Variáveis"""
 ## negrito
@@ -39,123 +40,110 @@ RED 		= BOLD+'\033[91m'
 ENDL 		= "\n"
 ## tabulação	
 TAB 		= "\t"
-## separador CSV	
-SEP 		= ";" 
-## dicionário
-MATCHES 	= {}
-## opção de ação do programa (-i, -t ou -x)		
+## opção de ação do programa	
 ARG_OPTION  = ""
-## diretório a ser aplicado a ação	
+## diretório/arquivo de entrada
 ARG_PATH 	= ""
-## nome do arquivo de rastreamento
-TMP 		= datetime.now().strftime('%Y%m%d%H%M%S')
-## extensão do arquivo de rastreamento
-EXT 		= ""
+## tamanho máximo do arquivo de entrada (1MB)
+MAX_SIZE 	= 1048576
+## nome do arquivo de saída
+OUTPUT 		= datetime.now().strftime('%Y%m%d%H%M%S')
+## extensão do arquivo de saída
+EXT 		= ".txt"
+## separador	
+SEP 		= " " 
 
+## lista de instruções permitidas
+INST_ALLOW 	= ['add', 'sub', 'beq', 'bne', 'lw', 'sw', 'j']
+## lista de instruções [linha => instrução]
+INST_LIST	= []
+## dicionário de instruções {(linha, [label, comando, oper1, oper2, oper3])}
+INST_DIC 	= {}
+## dicionário de labels {(label, linha)}
+LABEL_DIC	= {}
+## lista de registradores em uso
+REG_LOCK 	= []
 
 ## 	@brief 		Função para limpar console
 def clearConsole():
+	print 'Loading...'
+	sleep(0.5)
 	os.system('cls' if os.name == 'nt' else 'reset')
 
 
 ## 	@brief 		Função printExampleArgs()
 # 	@details 	Imprimir explicação dos argumentos via linha de comando
 def printExampleArgs():
-	print '%sGive an option and a folder:%s' 				% (ENDL, ENDL)
-	print '-i %sto start folder guard%s' 					% (TAB, ENDL)
-	print '-t %sto track the folder%s' 						% (TAB, ENDL)
-	print '-x %sto stop folder guard%s' 					% (TAB, ENDL)
-	print 'Example: ./guard.py <option> <folder_path> %s' 	% (ENDL)
-	print 'Ending... %s' 									% (ENDL)
+	print '%sEnter an argument!%s' 									% (ENDL, ENDL)
+	print 'Example: ./pipeline.py <folder_path>/<input_file> %s' 	% (ENDL)
+	print 'Ending... %s' 											% (ENDL)
 
 
 ## 	@brief 		Função checkArgs()
 # 	@details 	Validação dos argumentos passados via linha de comando
 def checkArgs():
 	global ARG_OPTION, ARG_PATH
-	if sys.argv.__len__() == 3:
-		ARG_OPTION 	= str(sys.argv[1])
-		ARG_PATH 	= str(sys.argv[2])
-		if ARG_OPTION not in ('-i', '-t', '-x'):
-			print 'Invalid option!%s' % (ENDL)
+	if sys.argv.__len__() == 2:
+		ARG_PATH 	= str(sys.argv[1])
+		if not loadInputFile(ARG_PATH):
 			printExampleArgs()
 			sys.exit()
 	else:
-		print 'Expected 2 arguments but %i was given!%s' % (sys.argv.__len__()-1, ENDL)
+		print 'Expect 1 argument but %i was given!%s' % (sys.argv.__len__()-1, ENDL)
 		printExampleArgs()
 		sys.exit()
 
 
-## 	@brief 		Função genHMAC()
-# 	@details 	Gera HMAC dos arquivos do diretório de forma recursiva
-# 	@param 		dir - caminho do diretório
-def genHMAC(dir):
-	global MATCHES
-	# mudando diretorio
-	os.chdir(dir)
-	# caminho absoluto para diretorio
-	dir = os.getcwd()
+## 	@brief		Função loadInputFile()
+# 	@details 	Carrega o arquivo de entrada
+# 	@param 		pathfile - arquivo
+# 	@return 	boolean
+def loadInputFile(pathfile):
+	global INST_LIST, INST_DIC, LABEL_DIC
+	# dados do arquivo
+	stats = os.stat(pathfile)
 
-	# se existe pasta oculta
-	if HIDDEN in os.listdir(dir):
-		# percorre recursivamente todos os subdiretórios
-		for root, dirnames, filenames in os.walk(dir):
-			# percore todos os arquivos dos subdiretórios
-			for filename in filenames:
-				# ignora a pasta oculta
-				if root[-len(HIDDEN):] != HIDDEN:
-					# caminho completo
-					fullpath_filename = os.path.join(root, filename)
-					# abre arquivo e lê conteúdo
-					f = open(fullpath_filename, "r")
-					if f:
-						content = f.read()
-						f.close()
-						# objeto HMAC - submete CAMINHO + CONTEUDO
-						h = HMAC_OBJ.hmac(fullpath_filename+content)
-						# h = HMAC_OBJ.hmac(content)
-						# adiciona caminho absoluto do arquivo e seu respectivo HMAC ao dicionário
-						MATCHES.update( {fullpath_filename : h.hexdigest()} )
-					else:
-						MATCHES.update( {fullpath_filename : 'Error trying to open file!'} )
+	# verifica o tamanho, se é arquivo e tenta abrir o arquivo de entrada
+	if stats.st_size <= MAX_SIZE and os.path.isfile(pathfile):
+		try:
+			input_file = open(pathfile, "r")
+		except IOError:
+			print "%sError trying to load input file!" % (ENDL)
+			return False
 
-		# ordenar dicionário por chave (fullpath_filename)
-		MATCHES = OrderedDict(sorted(MATCHES.items(), key=lambda t: t[0]))
+		# caso abriu o arquivo de entrada
+		with input_file:
+			for (line, instruction) in enumerate(input_file):
+				# testa se a instrução é permitida
+				if instruction.split(SEP)[0] in INST_ALLOW or ':' in instruction.split(SEP)[0]:
+					line += 1;
+					instruction = instruction.replace('\n', '')
+					# preenche a lista de instruções
+					INST_LIST.insert(line, instruction)
+					# preenche o dicionario de instruções
+					instruction = instruction.replace(',', '')
+					inst_parts = instruction.split(SEP)
+					INST_DIC[line] = inst_parts
+					# preenche o dicionário de labels
+					if ':' in inst_parts[0]:
+						LABEL_DIC[inst_parts[0]] = line
+				else:
+					print 'Instruction/Label not allowed: %s' % (instruction.split(SEP)[0])
+					print 'Allowed instructions: %s' % (INST_ALLOW)
+					print 'Labels must have collon symbol (:).'
+					return False
 
-		# Diretório inválido ou arquivos inexistentes
-		if MATCHES.__len__() == 0:
-			print 'Invalid folder or there are no files in the folder!%s' % (ENDL)
-			printExampleArgs()
-			sys.exit()
+			print INST_LIST
+			print INST_DIC
+			print LABEL_DIC
+			print len(INST_DIC)
+
+			input_file.close()
+			print "%sInput file loaded: %s" % ( ENDL, pathfile )
+			return True
 	else:
-		print "%sFolder '%s' not found! You need to start folder guard: %s" % (ENDL, HIDDEN, os.getcwd())
-		printExampleArgs()
-		sys.exit()
-		
-
-## 	@brief		Função createTrackFile()
-# 	@details 	Criar arquivo de rastreio contendo os arquivos e os respectivos HMACs
-# 	@param 		dir - caminho do diretório
-def createTrackFile(dir):
-	
-	# se existe pasta oculta
-	if HIDDEN in os.listdir(dir):
-		# mudando para pasta oculta
-		os.chdir(dir+'/'+HIDDEN)
-		print "%sGenerating new tracking file: %s" % ( ENDL, TMP+EXT )
-		# salva dados do dicionário no arquivo de rastreio
-		track_file = open(TMP+EXT, "w+")
-		if track_file:
-			for filename, hash in MATCHES.iteritems():
-				track_file.write( str(hash+SEP+filename+ENDL) )
-			track_file.close()
-			print "%sTracking file saved in: %s" % ( ENDL, os.getcwd() )
-		else:
-			print "%sError trying to generate tracking file!" % (ENDL)
-	else:
-		print "%sFolder '%s' not found! You need to start folder guard: %s" % (ENDL, HIDDEN, os.getcwd())
-		printExampleArgs()
-
+		print "%sError trying to load input file!" % (ENDL)
+		return False
 
 
 # BLOCO PRINCIPAL DO PROGRAMA
@@ -164,8 +152,8 @@ if __name__ == "__main__":
 	# limpar console
 	clearConsole()
 
-	print "%s==> Simulador Pipeline" % (WHITE)
-	print "    Harvard's Arquitecture.%s%s" % (ENDL, NORMAL)
+	print "%s==> Pipeline Simulator" % (GREEN)
+	print "    Harvard Architecture.%s%s" % (ENDL, NORMAL)
 
 	# validar argumentos
 	checkArgs()
