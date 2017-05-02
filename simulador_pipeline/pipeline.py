b49e73d1cb1ab1211531ffd83c21074fea0358a9
davidcardoso-ti <davidcardoso.ti@gmail.com>
2017-05-02 02:29:29 -0300

    - identificando dependencias - tratando jumps (sempre salta para o label) - inserindo bolhas (NOP) quando há conflito de dados - salvando situação dos estágios do pipeline em um arquivo de saída - imprimindo na tela quantidade total de ciclos - imprimindo na tela situações dos estágios do pipeline

--- simulador_pipeline/pipeline.py
+++ simulador_pipeline/pipeline.py
@@ -21,48 +21,66 @@ from datetime import datetime			# datetime 		- data e tempo
 from time import sleep 					# sleep 		- pausa a execução da thread atual
 
 """Variáveis"""
-## negrito
+# negrito
 BOLD 		= '\033[1m'
-## cor padrão para impressões no terminal		
+# cor padrão para impressões no terminal		
 NORMAL 		= '\033[0;0m'		
-## cor verde para destacar impressões no terminal
+# cor verde para destacar impressões no terminal
 GREEN 		= BOLD+'\033[32m'	
-## cor azul para destacar impressões no terminal
+# cor azul para destacar impressões no terminal
 BLUE 		= BOLD+'\033[34m'	
-## cor branca para destacar impressões no terminal
+# cor branca para destacar impressões no terminal
 WHITE 		= BOLD+'\033[37m'	
-## cor amarela para destacar impressões no terminal
+# cor amarela para destacar impressões no terminal
 YELLOW 		= BOLD+'\033[93m'	
-## cor vermelha para destacar impressões no terminal
+# cor vermelha para destacar impressões no terminal
 RED 		= BOLD+'\033[91m' 
 
-## quebra de linha
+# quebra de linha
 ENDL 		= "\n"
-## tabulação	
+# tabulação	
 TAB 		= "\t"
-## opção de ação do programa	
+# opção de ação do programa	
 ARG_OPTION  = ""
-## diretório/arquivo de entrada
+# diretório/arquivo de entrada
 ARG_PATH 	= ""
-## tamanho máximo do arquivo de entrada (1MB)
+# tamanho máximo do arquivo de entrada (1MB)
 MAX_SIZE 	= 1048576
-## nome do arquivo de saída
+# nome do arquivo de saída
 OUTPUT 		= datetime.now().strftime('%Y%m%d%H%M%S')
-## extensão do arquivo de saída
+# extensão do arquivo de saída
 EXT 		= ".txt"
-## separador	
+# separador	
 SEP 		= " " 
 
-## lista de instruções permitidas
+# lista de instruções permitidas
 INST_ALLOW 	= ['add', 'sub', 'beq', 'bne', 'lw', 'sw', 'j']
-## lista de instruções [linha => instrução]
-INST_LIST	= []
-## dicionário de instruções {(linha, [label, comando, oper1, oper2, oper3])}
+# lista de instruções [linha => instrução]
+INST_LIST	= ['']
+# dicionário de instruções {linha : [label, comando, oper1, oper2, oper3]}
 INST_DIC 	= {}
-## dicionário de labels {(label, linha)}
+# dicionário de labels {label : linha}
 LABEL_DIC	= {}
-## lista de registradores em uso
+# lista de registradores em uso
 REG_LOCK 	= []
+# dicionário de estágios do pipeline
+PIPE_STAGES = {'IF' : '0', 'ID' : '0', 'EX' : '0', 'MEM' : '0', 'WB' : '0'}
+# dicionário de regras de dependências 
+# instrução anterior (parent) é necessária para a seguinte (child) nos operadores 2, 3 e/ou 4
+INST_DEP	= {
+	'add' 	: {'add':[3,4], 'sub':[3,4], 'lw':[3], 'sw':[2], 'beq':[2,3], 'bne':[2,3],},
+	'sub' 	: {'add':[3,4], 'sub':[3,4], 'lw':[3], 'sw':[2], 'beq':[2,3], 'bne':[2,3],},
+	'lw'	: {'add':[3,4], 'sub':[3,4], 'beq':[2,3], 'bne':[2,3],},
+	}
+# instruções de salto (jump)
+INST_JUMPS 	= ['beq', 'bne', 'j']
+# contador de programa
+PC 			= 1
+# quantidade de instruções
+INST_QTD 	= 0
+# quantidade de ciclos (clocks)
+CLOCKS 		= 0
+
 
 ## 	@brief 		Função para limpar console
 def clearConsole():
@@ -99,7 +117,7 @@ def checkArgs():
 # 	@param 		pathfile - arquivo
 # 	@return 	boolean
 def loadInputFile(pathfile):
-	global INST_LIST, INST_DIC, LABEL_DIC
+	global INST_LIST, INST_DIC, LABEL_DIC, INST_QTD
 	# dados do arquivo
 	stats = os.stat(pathfile)
 
@@ -118,32 +136,110 @@ def loadInputFile(pathfile):
 				if instruction.split(SEP)[0] in INST_ALLOW or ':' in instruction.split(SEP)[0]:
 					line += 1;
 					instruction = instruction.replace('\n', '')
+					instruction = instruction.replace('$zero', '$00')
 					# preenche a lista de instruções
 					INST_LIST.insert(line, instruction)
 					# preenche o dicionario de instruções
 					instruction = instruction.replace(',', '')
 					inst_parts = instruction.split(SEP)
-					INST_DIC[line] = inst_parts
+					if inst_parts[0] in ['lw', 'sw']:
+						inst_parts[2] = str(inst_parts[2])[-4:-1]
 					# preenche o dicionário de labels
 					if ':' in inst_parts[0]:
-						LABEL_DIC[inst_parts[0]] = line
+						label = inst_parts[0].replace(':','')
+						LABEL_DIC[label] = line
+						INST_DIC[line] = [label]+inst_parts[1:]
+					else:
+						INST_DIC[line] = ['']+inst_parts
+
+					INST_QTD += 1
 				else:
 					print 'Instruction/Label not allowed: %s' % (instruction.split(SEP)[0])
 					print 'Allowed instructions: %s' % (INST_ALLOW)
 					print 'Labels must have collon symbol (:).'
 					return False
 
-			print INST_LIST
-			print INST_DIC
-			print LABEL_DIC
-			print len(INST_DIC)
+			# print INST_LIST
+			# print INST_DIC
+			# print LABEL_DIC
 
 			input_file.close()
-			print "%sInput file loaded: %s" % ( ENDL, pathfile )
-			return True
+
+			if INST_QTD == 0:
+				print 'Input file has no instructions to be processed!'
+				return False
+			else:
+				print "%sInput file loaded: %s%s" % ( ENDL, pathfile, ENDL )
+				return True
 	else:
 		print "%sError trying to load input file!" % (ENDL)
 		return False
+
+## 	@brief 		Função identifyDeps()
+# 	@details 	Identifica as dependências
+# 	@param 		reg - registrador base para verificação de dependência
+# 	@return 	dicionário com as dependências
+def identifyDeps(reg):
+	hasDep = {}
+	for (line, parent) in INST_DIC.iteritems():
+		if parent[1] in INST_DEP:
+			for (key, child) in INST_DIC.iteritems():
+				if key > line:
+					deps = INST_DEP.get(parent[1], 0)
+					opers = deps.get(child[1], 0)
+					if type(opers) == list:
+						for i in opers:
+							if child[i] == parent[reg]:
+								# print "linha %s depende da linha %s" % (key, line)
+								if key in hasDep:
+									hasDep[key].append(line)
+								else:
+									hasDep[key] = [line]
+	return hasDep
+
+
+## 	@brief 		Função checkJump()
+# 	@details 	Verifica se há jump para configurar corretamente o próximo PC
+# 	@param 		pc - program counter atual
+# 	@return 	próximo PC
+def checkJump(pc):
+	jump = INST_DIC[pc][1]
+	if jump in INST_JUMPS:
+		label = INST_DIC[pc][-1]
+		return LABEL_DIC[label]
+	else:
+		return pc + 1
+
+
+## 	@brief 		Função printPipelineClock()
+#	@details	Imprime as instruções que estão nos estágios do pipeline em determinado clock
+#	@param 		clocks - número do clock atual
+#	@param 		stages - estágios do pipeline
+def printPipelineClock(clocks, stages):
+	print '--------------------------------------------'
+	print 'Clock #%s:' % (clocks)
+	print 'IF:%s%s' % (TAB, INST_LIST[int(stages['IF'])] if type(stages['IF']) == int else stages['IF'].replace('end','0'))
+	print 'ID:%s%s' % (TAB, INST_LIST[int(stages['ID'])] if type(stages['ID']) == int else stages['ID'].replace('end','0'))
+	print 'EX:%s%s' % (TAB, INST_LIST[int(stages['EX'])] if type(stages['EX']) == int else stages['EX'].replace('end','0'))
+	print 'MEM:%s%s' % (TAB, INST_LIST[int(stages['MEM'])] if type(stages['MEM']) == int else stages['MEM'].replace('end','0'))
+	print 'WB:%s%s' % (TAB, INST_LIST[int(stages['WB'])] if type(stages['WB']) == int else stages['WB'].replace('end','0'))
+
+
+## 	@brief 		Função writePipelineOutput()
+#	@details	Escreve as instruções que estão nos estágios do pipeline em um arquivo de saída
+#	@param 		clocks - número do clock atual
+#	@param 		stages - estágios do pipeline
+def writePipelineOutput(clocks, stages):
+	file = open(OUTPUT+EXT, 'a')
+	out = '--------------------------------------------\n'
+	out += 'Clock #%s:\n' % (clocks)
+	out += 'IF:%s%s\n' % (TAB, INST_LIST[int(stages['IF'])] if type(stages['IF']) == int else stages['IF'].replace('end','0'))
+	out += 'ID:%s%s\n' % (TAB, INST_LIST[int(stages['ID'])] if type(stages['ID']) == int else stages['ID'].replace('end','0'))
+	out += 'EX:%s%s\n' % (TAB, INST_LIST[int(stages['EX'])] if type(stages['EX']) == int else stages['EX'].replace('end','0'))
+	out += 'MEM:%s%s\n' % (TAB, INST_LIST[int(stages['MEM'])] if type(stages['MEM']) == int else stages['MEM'].replace('end','0'))
+	out += 'WB:%s%s\n' % (TAB, INST_LIST[int(stages['WB'])] if type(stages['WB']) == int else stages['WB'].replace('end','0'))
+	file.write(out)
+	file.close()
 
 
 # BLOCO PRINCIPAL DO PROGRAMA
@@ -153,9 +249,61 @@ if __name__ == "__main__":
 	clearConsole()
 
 	print "%s==> Pipeline Simulator" % (GREEN)
-	print "    Harvard Architecture.%s%s" % (ENDL, NORMAL)
+	print "    Harvard Architecture%s" % (ENDL)
+	print "    Stages: IF, ID, EX, MEM, WB%s%s" % (ENDL, NORMAL)
 
-	# validar argumentos
+	# validar argumentos passados via linha de comando
 	checkArgs()
 
+	# identificar dependências
+	hasDep = identifyDeps(2)
+
+	# processamento das instruções
+	while PIPE_STAGES['MEM'] != 'end':
+		# contador de clocks
+		CLOCKS += 1
+
+		# segue o processamento das instruções através dos estágios do pipeline
+		PIPE_STAGES['WB'] = PIPE_STAGES['MEM']
+		PIPE_STAGES['MEM'] = PIPE_STAGES['EX']
+		PIPE_STAGES['EX'] = PIPE_STAGES['ID']
+		PIPE_STAGES['ID'] = PIPE_STAGES['IF']
+
+		# verifica se ainda tem mais instruções a serem executadas
+		if PC <= INST_QTD:
+			depend = False
+			# verifica se a instrução atual tem dependência no pipeline
+			if PC in hasDep:
+				for dep in hasDep[PC]:
+					if depend:
+						break
+					for (stage, line) in PIPE_STAGES.iteritems():
+						# insere bolha, se alguma instrução parent estiver sendo processada
+						if stage != 'WB' and dep == line:
+							PIPE_STAGES['IF'] = 'NOP'
+							depend = True
+							break;
+						else:
+						# insere a instrução PC no pipeline, se a instrução parent estiver no estágio WB
+							if stage == 'WB' and dep == line:
+								depend = False
+			if not depend:
+				PIPE_STAGES['IF'] = PC
+				PC = checkJump(PC)
+		else:
+			PIPE_STAGES['IF'] = 'end'
+
+		# situação atual do pipeline
+		# printPipelineClock(CLOCKS, PIPE_STAGES)
+		writePipelineOutput(CLOCKS, PIPE_STAGES)
+
+	# impressão do total de ciclos
+	print 'Total number of clocks: %s' % CLOCKS
+
+ 	# impressão das situações de cada ciclo do pipeline
+	file = open(OUTPUT+EXT, 'r')
+	print file.read()
+	file.close()
+
+	# fim do programa
 	sys.exit()
